e766ddfd7d13500271e3810e5cc929591b5f5b10
Tsairenkun <Tsairenkun@gmail.com>
2020-02-27 17:50:41 +0800

    can add can delete

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -9,21 +9,62 @@ constructor(){
 
   this.state = {
     thingsTodo : "",
-    ToDoList : [],
+    ToDoList : []
   }
 }
 
-clickHandler(number) {
-    console.log("DO DO DO")
+clickTodoHandler() {
+
+    let currentword = this.state.thingsTodo
+    let array = this.state.ToDoList
+if (currentword){
+    array.push(currentword)
+
+    this.setState(array)
+
+    this.refs.input.value = "";
+    this.state.thingsTodo = "";
+    }
+}
+
+todoHandler(event){
+this.state.thingsTodo = event.target.value
 }
 
 
+deleteItem(key){
+    const filteredItems = this.state.ToDoList.filter((item) => {
+      return this.state.ToDoList[key] !== item
+    })
+    this.setState({
+      ToDoList: filteredItems,
+    })
+  }
 
   render() {
+
+    let showList = this.state.ToDoList.map((stuff, index)=>{
+        return(
+            <div>
+            <li key={index}>{stuff}
+            <button key={index} onClick={() => this.deleteItem(index)}>Delete</button>
+            </li>
+            </div>
+            )
+    })
+
     return (
-      <div>
+      <div >
         Welcome.
-        <button onClick={()=>{this.clickHandler()}}>Add a "To Do"</button>
+        <div>
+        <input ref= "input" onChange={(event)=>{this.todoHandler(event);}}/>
+        <button onClick={()=>{this.clickTodoHandler()}}>Add a "To Do"</button>
+        </div>
+        <div >
+            <ul>
+            {showList}
+            </ul>
+        </div>
       </div>
     );
   }
