72af1d1b9689f09d4df6cc5248011d3240d28066
Tsairenkun <Tsairenkun@gmail.com>
2020-02-27 16:44:10 +0800

    added a button

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+
+constructor(){
+  super()
+  console.log("constructing");
+
+  this.state = {
+    thingsTodo : "",
+    ToDoList : [],
+  }
+}
+
+clickHandler(number) {
+    console.log("DO DO DO")
+}
+
+
+
   render() {
     return (
       <div>
         Welcome.
+        <button onClick={()=>{this.clickHandler()}}>Add a "To Do"</button>
       </div>
     );
   }
